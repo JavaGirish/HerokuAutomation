c020782b89a680bf2fb01117d96206c2fa3230a9
Girish Nair <nairgirish100@gmail.com>
2019-09-08 16:50:00 +0530

    headless changes updated

--- UIAutomation/BackBase/src/main/java/com/backbase/pageobject/Setup.java
+++ UIAutomation/BackBase/src/main/java/com/backbase/pageobject/Setup.java
@@ -1,15 +1,14 @@
 package com.backbase.pageobject;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Setup 
 {
@@ -24,7 +23,10 @@ public WebDriver initializeDriver () throws IOException
 	if (browserName.equals("Chrome")) 
 	{
 		System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
-		driver = new ChromeDriver();
+		ChromeOptions op= new ChromeOptions();
+		op.addArguments("window-size=1400,800");
+		op.addArguments("headless");
+		driver = new ChromeDriver(op);
 	}
 	else if (browserName.equals("FireFox"))
 	{
@@ -32,7 +34,7 @@ public WebDriver initializeDriver () throws IOException
 		driver = new FirefoxDriver();
 	}
 	driver.manage().window().maximize();
-	driver.manage().timeouts().pageLoadTimeout(4, TimeUnit.MINUTES);
+	driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.MINUTES);
 	driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);	// Wait to load the browser 
 	return driver;
 }
